2fe55b563ab372a63db60ce8d4044766db0088e4
prateekvarma <prtk.varma@gmail.com>
2023-06-06 14:33:24 -0600

    Test for other elements in the repo

--- src/components/repositories/RepositoriesSummary.test.js
+++ src/components/repositories/RepositoriesSummary.test.js
@@ -1,7 +1,7 @@
 import { screen, render } from '@testing-library/react';
 import RepositoriesSummary from './RepositoriesSummary';
 
-test('displays the primary langauage of the repository', () => {
+test('displays information about the repository', () => {
   const repository = {
     language: 'JavaScript',
     stargazers_count: 5,
@@ -11,7 +11,10 @@ test('displays the primary langauage of the repository', () => {
 
   render(<RepositoriesSummary repository={repository} />);
 
-  const language = screen.getByText('JavaScript');
+  for (let key in repository) {
+    const value = repository[key];
+    const element = screen.getByText(new RegExp(value));
 
-  expect(language).toBeInTheDocument();
+    expect(element).toBeInTheDocument();
+  }
 });
