012422891d06d9841d7f72c92f7b1cd9ed8a257b
prateekvarma <prtk.varma@gmail.com>
2023-07-13 12:29:58 -0600

    Refactored MSW so it could be re-used

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -3,36 +3,22 @@ import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
+import { createServer } from '../test/server';
 
-const handlers = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const language = req.url.searchParams.get('q').split('language:')[1];
-    //console.log(language);
-
-    return res(
-      ctx.json({
+createServer([
+  {
+    path: '/api/repositories',
+    res: (req, res, ctx) => {
+      const language = req.url.searchParams.get('q').split('language:')[1];
+      return {
         items: [
           { id: 1, full_name: `${language}_one` },
           { id: 2, full_name: `${language}_two` },
         ],
-      })
-    );
-  }),
-];
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      };
+    },
+  },
+]);
 
 test('renders two links for each language', async () => {
   render(
