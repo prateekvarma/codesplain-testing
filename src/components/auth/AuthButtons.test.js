cb3bed4b7eb09d4e68028dcd5038e4a541c2999c
prateekvarma <prtk.varma@gmail.com>
2023-07-17 14:05:46 -0600

    Test case to show signin/signup buttons when user is not logged in

--- src/components/auth/AuthButtons.test.js
+++ src/components/auth/AuthButtons.test.js
@@ -3,6 +3,15 @@ import { MemoryRouter } from 'react-router';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
+async function renderComponent() {
+  render(
+    <MemoryRouter>
+      <AuthButtons />
+    </MemoryRouter>
+  );
+  await screen.findAllByRole('link');
+}
+
 describe('When the user is not signed in', () => {
   createServer([
     {
@@ -13,22 +22,38 @@ describe('When the user is not signed in', () => {
     },
   ]);
 
-  test('when user is not signed in, sign in and sign up are visible', async () => {});
+  test('when user is NOT signed in, sign in and sign up are visible', async () => {
+    await renderComponent();
 
-  test('when user is not signed in, sign out is not visible', async () => {});
+    const signInButton = screen.getByRole('link', { name: /sign in/i });
+    const signUpButton = screen.getByRole('link', { name: /sign up/i });
+
+    expect(signInButton).toBeInTheDocument();
+    expect(signInButton).toHaveAttribute('href', '/signin');
+    expect(signUpButton).toBeInTheDocument();
+    expect(signUpButton).toHaveAttribute('href', '/signup');
+  });
+
+  test('when user is NOT signed in, sign out is not visible', async () => {
+    await renderComponent();
+  });
 });
 
-describe('When the user is signed in', () => {
-  createServer([
-    {
-      path: '/api/user',
-      res: () => {
-        return { user: { id: 3, email: 'asdf@asdf.com' } };
-      },
-    },
-  ]);
+// describe('When the user is signed in', () => {
+//   createServer([
+//     {
+//       path: '/api/user',
+//       res: () => {
+//         return { user: { id: 3, email: 'asdf@asdf.com' } };
+//       },
+//     },
+//   ]);
 
-  test('when user is signed in, sign in and sign up are not visible', async () => {});
+//   test('when user IS signed in, sign in and sign up are not visible', async () => {
+//     renderComponent();
+//   });
 
-  test('when user is signed in, sign out is visible', async () => {});
-});
+//   test('when user IS signed in, sign out is visible', async () => {
+//     renderComponent();
+//   });
+// });
